278ddd589871f7d6e1f11fb560f992699b07e984
Artemiy Levkevich <a_gakh1@mail.ru>
2025-03-02 14:39:06 +0800

    Fix View + relocate js/css + add Vite

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+let visitDuration = 0;
+setInterval(() => visitDuration++, 1000);
+document.querySelector('form').addEventListener('submit', () => {
+    let input = document.createElement('input');
+    input.type = 'hidden';
+    input.name = 'visit_duration';
+    input.value = visitDuration > 30 ? 1 : 0;
+    document.querySelector('form').appendChild(input);
+});
