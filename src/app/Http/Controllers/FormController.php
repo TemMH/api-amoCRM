76344b67e6c72909c71d93844588e3aefe9680f7
Artemiy Levkevich <a_gakh1@mail.ru>
2025-03-03 03:37:47 +0800

    relocate validate -> request & remove model/factory/migrations/seeder

--- src/app/Http/Controllers/FormController.php
+++ src/app/Http/Controllers/FormController.php
@@ -14,7 +14,7 @@ use AmoCRM\Models\CustomFieldsValues\MultitextCustomFieldValuesModel;
 use AmoCRM\Models\CustomFieldsValues\ValueCollections\MultitextCustomFieldValueCollection;
 use AmoCRM\Models\CustomFieldsValues\ValueModels\MultitextCustomFieldValueModel;
 use AmoCRM\Models\LeadModel;
-
+use App\Http\Requests\StoreLeadRequest;
 
 class FormController extends Controller
 {
@@ -26,91 +26,83 @@ class FormController extends Controller
         $this->apiClient = $apiClient;
     }
 
-    public function store(Request $request)
+    public function store(StoreLeadRequest $request)
     {
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
-            'phone' => 'required|string|max:20',
-            'price' => 'required|numeric',
-            'visit_duration' => 'nullable|integer|min:0'
-        ]);
-    
+        $validatedData = $request->validated();
+
         $contact = $this->addContact($validatedData['name'], $validatedData['phone'], $validatedData['email']);
-        $lead = $this->addLead($validatedData['price'],$validatedData['visit_duration'], $contact);
-        
+        $lead = $this->addLead($validatedData['price'], $validatedData['visit_duration'], $contact);
+
         return back();
     }
-    
+
     private function addContact($name, $phone, $email)
     {
         $contact = new ContactModel();
         $contact->setName($name);
-    
+
         $phoneField = new MultitextCustomFieldValuesModel();
         $phoneField->setFieldCode('PHONE')
             ->setValues((new MultitextCustomFieldValueCollection())
-                ->add((new MultitextCustomFieldValueModel())
-                    ->setEnum('WORK')
-                    ->setValue($phone)
-                )
+                    ->add((new MultitextCustomFieldValueModel())
+                            ->setEnum('WORK')
+                            ->setValue($phone)
+                    )
             );
-    
+
         $emailField = new MultitextCustomFieldValuesModel();
         $emailField->setFieldCode('EMAIL')
             ->setValues((new MultitextCustomFieldValueCollection())
-                ->add((new MultitextCustomFieldValueModel())
-                    ->setEnum('WORK')
-                    ->setValue($email)
-                )
+                    ->add((new MultitextCustomFieldValueModel())
+                            ->setEnum('WORK')
+                            ->setValue($email)
+                    )
             );
-    
+
         $customFieldsValues = new CustomFieldsValuesCollection();
         $customFieldsValues->add($phoneField);
         $customFieldsValues->add($emailField);
-    
+
         $contact->setCustomFieldsValues($customFieldsValues);
-    
+
         try {
             return $this->apiClient->contacts()->addOne($contact);
         } catch (AmoCRMApiException $e) {
             return response()->json(['error' => $e->getMessage()], 422);
         }
     }
-    
 
-private function addLead($price, $visit_duration, $contact)
-{
-    $lead = new LeadModel();
-    $lead->setName("Сделка №" . uniqid());
-    $lead->setPrice($price);
 
-    $customFieldsValues = new CustomFieldsValuesCollection();
+    private function addLead($price, $visit_duration, $contact)
+    {
+        $lead = new LeadModel();
+        $lead->setName("Сделка №" . uniqid());
+        $lead->setPrice($price);
 
-    $booleanValue = ($visit_duration == 1);
+        $customFieldsValues = new CustomFieldsValuesCollection();
 
-    $customField = new CheckboxCustomFieldValuesModel();
-    $customField->setFieldId(744461)
-        ->setValues((new CheckboxCustomFieldValueCollection())
-            ->add((new CheckboxCustomFieldValueModel())
-                ->setValue($booleanValue)
-            )
-        );
+        $booleanValue = ($visit_duration == 1);
 
-    $customFieldsValues->add($customField);
+        $customField = new CheckboxCustomFieldValuesModel();
+        $customField->setFieldId(744461)
+            ->setValues((new CheckboxCustomFieldValueCollection())
+                    ->add((new CheckboxCustomFieldValueModel())
+                            ->setValue($booleanValue)
+                    )
+            );
 
-    $lead->setCustomFieldsValues($customFieldsValues);
+        $customFieldsValues->add($customField);
 
-    $contactsCollection = new \AmoCRM\Collections\ContactsCollection();
-    $contactsCollection->add($contact);
-    $lead->setContacts($contactsCollection);
+        $lead->setCustomFieldsValues($customFieldsValues);
 
-    try {
-        return $this->apiClient->leads()->addOne($lead);
-    } catch (AmoCRMApiException $e) {
-        return response()->json(['error' => $e->getMessage()], 422);
+        $contactsCollection = new \AmoCRM\Collections\ContactsCollection();
+        $contactsCollection->add($contact);
+        $lead->setContacts($contactsCollection);
+
+        try {
+            return $this->apiClient->leads()->addOne($lead);
+        } catch (AmoCRMApiException $e) {
+            return response()->json(['error' => $e->getMessage()], 422);
+        }
     }
 }
-
-}
-
--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -64,6 +64,16 @@
                                 </p>
                             </span>
                         </div>
+
+                        @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
                     </div>
                 </div>
             </div>
